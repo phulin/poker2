fixes to denominator calculation

src/alphaholdem/search/rebel_cfr_evaluator.py
@@ -905,13 +905,14 @@ class RebelCFREvaluator:
 
         # Reach probability is proportional to belief, so we can use beliefs to mix
         weight = 2 if self.linear_cfr else 1
+        reach_actor *= weight
+        reach_avg_actor *= t
         self.policy_probs_avg *= reach_avg_actor
-        self.policy_probs_avg *= t
-        self.policy_probs_avg += weight * self.policy_probs * reach_actor
+        self.policy_probs_avg += self.policy_probs * reach_actor
+        denom = reach_avg_actor + reach_actor
         torch.where(
-            self.policy_probs_avg.abs() > 1e-8,
-            self.policy_probs_avg
-            / (t * reach_avg_actor + weight * reach_actor).clamp(min=1e-8),
+            denom > 1e-8,
+            self.policy_probs_avg / denom.clamp(min=1e-8),
             torch.zeros_like(self.policy_probs_avg),
             out=self.policy_probs_avg,
         )