c781e4771de62552f0d9e094571e0646172f8f37
Patrick Hulin <patrick.hulin@gmail.com>
2025-11-09 15:49:17 -0500

    add street to features...

--- src/alphaholdem/models/mlp/better_features.py
+++ src/alphaholdem/models/mlp/better_features.py
@@ -4,10 +4,11 @@ from enum import Enum
 class ScalarContext(Enum):
     ACTOR = 0
     POSITION = 1
-    ACTIONS_ROUND = 2
-    POT = 3
-    MIN_RAISE = 4
-    NUM_SCALAR_CONTEXT = 5
+    STREET = 2
+    ACTIONS_ROUND = 3
+    POT = 4
+    MIN_RAISE = 5
+    NUM_SCALAR_CONTEXT = 6
 
 
 class PlayerContext(Enum):
