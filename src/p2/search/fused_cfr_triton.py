4bafb9d22961ec3182fbd37a4dc7e2a57d2489e1
Patrick Hulin <patrick.hulin@gmail.com>
2026-05-19 00:31:19 +0000

    Tune model value writeback launch

--- src/p2/search/fused_cfr_triton.py
+++ src/p2/search/fused_cfr_triton.py
@@ -2373,7 +2373,7 @@ def fused_model_values_writeback_(
         ENFORCE_ZS=enforce_zero_sum,
         STORE_LAST=store_last,
         BLOCK_H=block_h,
-        num_warps=8,
+        num_warps=8 if (do_mix or enforce_zero_sum) else 4,
     )
 
 
